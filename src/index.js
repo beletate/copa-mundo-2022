32840eec98c8c770e53391959dfdd93346fe5df8
Vinicius <vinicius_beletate@hotmail.com>
2021-10-16 17:05:16 -0300

    Random port

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use('/team', teamRouter)
 app.use('/player', playerRouter)
 
-const port = 8087
+/* const port = 8087
 app.listen(port, () => {
   console.log(`Server started on port: ${port}`)
-})
+}) */

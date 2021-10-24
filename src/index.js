9f228011d3fd11143e80d73e1a71b1dce7980d37
Vinicius <vinicius_beletate@hotmail.com>
2021-10-23 21:52:28 -0300

    Retornando valor para http get /

--- src/index.js
+++ src/index.js
@@ -8,10 +8,12 @@ const app = express()
 app.use(express.json())
 app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
+app.get('/', (req, res) => {
+  res.send('Copa do mundo')
+})
 app.use('/team', teamRouter)
 app.use('/player', playerRouter)
 
-/* const port = 8087
 app.listen(port, () => {
-  console.log(`Server started on port: ${port}`)
-}) */
+  console.log(`Server started.`)
+})
